ea1db51bc4799b5457fe8eee85e03c7e978344ce
Koby <mcguirej2019@gmail.com>
2022-11-02 18:45:08 -0400

    Add boxes to the DOM using js
    The squares that will be used to draw were added in to the website, along
    with styling that centers them.

--- java.js
+++ java.js
@@ -1,1 +1,8 @@
+ let container = document.querySelector(".container");
+
+ for (i = 0; i < 16; i++) {
+    console.log("count")
+    let box = container.appendChild(document.createElement("div"));
+    box.className = "box";
+ }
  
