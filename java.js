56fdc601fc658233ddaf84bf257448ab1725e755
Koby <mcguirej2019@gmail.com>
2022-11-03 12:53:17 -0400

    Added JS code to dynamically change size of grid
    Changed the "grid layout" to use display: grid because it makes
    it easier to get user input and then plug that in to create an evenly spaced
    grid compared to flexbox. This in turn broke the actual painting function.

--- java.js
+++ java.js
@@ -1,16 +1,34 @@
+
  let container = document.querySelector(".container");
-
- for (i = 0; i < 16; i++) {
-    console.log("count")
-    let box = container.appendChild(document.createElement("div"));
-    box.className = "box";
- }
+ let button = document.querySelector(".button");
  
- let boxes = document.querySelectorAll(".box");
+ 
+ function changeBoxSize (userInput) {
+   container.textContent = ''
+   for (i = 0; i < userInput; i++) {
+      let box = container.appendChild(document.createElement("div"));
+      box.className = "box";
+   }
+ }
 
+
+ 
+
+
+ // Adding Event listeners to boxes
+ let boxes = document.querySelectorAll(".box");
  for (const box of boxes) {
     box.addEventListener("click", function onClick(){
         console.log("Box clicked");
         box.style.backgroundColor = "black";
     })
  }
+
+ //Changing the box size according to user input
+ button.addEventListener("click", function() {
+   let userInput = window.prompt("What would you like to change the size to?");
+   let area = Math.sqrt(userInput);
+   changeBoxSize(userInput);
+   container.style.gridTemplateColumns = `repeat(${area}, 1fr)`;
+   container.style.gridTemplateRows = `repeat(${area}, 1fr)`;
+ })
