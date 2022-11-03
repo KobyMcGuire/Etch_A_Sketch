33e5ac3d730345a6eab8cb8f0725518c970d9de4
Koby <mcguirej2019@gmail.com>
2022-11-03 15:02:13 -0400

    Fix the "painting" tool
    Fixed the breakage of the actual painting of the boxes by putting the
    for loop that was adding event listeners in an actual function and calling
    this fucntion after every time the grid was updated to a new size.

--- java.js
+++ java.js
@@ -3,19 +3,19 @@
  let button = document.querySelector(".button");
  
  
+
+ //Changing the amount of boxes in the grid
  function changeBoxSize (userInput) {
    container.textContent = ''
    for (i = 0; i < userInput; i++) {
       let box = container.appendChild(document.createElement("div"));
       box.className = "box";
    }
+   updateBoxes();
  }
 
-
- 
-
-
  // Adding Event listeners to boxes
+ function updateBoxes() {
  let boxes = document.querySelectorAll(".box");
  for (const box of boxes) {
     box.addEventListener("click", function onClick(){
@@ -23,6 +23,7 @@
         box.style.backgroundColor = "black";
     })
  }
+}
 
  //Changing the box size according to user input
  button.addEventListener("click", function() {
