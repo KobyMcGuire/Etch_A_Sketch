8c8aac8ece015f950ce3183e84b868844504073a
Koby <mcguirej2019@gmail.com>
2022-11-03 15:25:32 -0400

    Add stylization and a "Clear" button to the DOM
    Added a backgrouind color to the body in order to distinguish between
    graph and backround. A "Clear" buttonw was added so that the user can
    clear all color that he or she has put on the grid.

--- java.js
+++ java.js
@@ -1,11 +1,12 @@
 
  let container = document.querySelector(".container");
- let button = document.querySelector(".button");
+ let buttonSize = document.querySelector(".buttonSize");
+ let buttonClear = document.querySelector(".buttonClear");
  
  
 
  //Changing the amount of boxes in the grid
- function changeBoxSize (userInput) {
+ function changeBoxAmount (userInput) {
    container.textContent = ''
    for (i = 0; i < userInput; i++) {
       let box = container.appendChild(document.createElement("div"));
@@ -25,11 +26,23 @@
  }
 }
 
+//Helper function to clear the color in boxes
+function clearColor(item) {
+  item.style.backgroundColor = "white";
+}
+
  //Changing the box size according to user input
- button.addEventListener("click", function() {
+ buttonSize.addEventListener("click", function changeBoxSize() {
    let userInput = window.prompt("What would you like to change the size to?");
    let area = Math.sqrt(userInput);
-   changeBoxSize(userInput);
+   changeBoxAmount(userInput);
    container.style.gridTemplateColumns = `repeat(${area}, 1fr)`;
    container.style.gridTemplateRows = `repeat(${area}, 1fr)`;
+ })
+
+
+ //Clearing coloring using event listeners
+ buttonClear.addEventListener("click", function clear() {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach(clearColor);
  })
